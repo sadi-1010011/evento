60c2a410a40558cd2b1c19e68e707ad7992bc5dc
sadiq <sadi1010011@gmail.com>
2024-10-14 14:49:39 +0530

    domain changed in test page too

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -2,7 +2,7 @@ import connectMongoDB from "@/lib/db";
 import Event from "@/models/event";
 
 export async function generateStaticParams() {
-    const events = await fetch(`https://evento-calicut.vercel.app/api/events`, { method: 'GET' }).then(res => res.json());
+    const events = await fetch(`https://thepluto.xyz/api/events`, { method: 'GET' }).then(res => res.json());
    
     return events.map((event: any) => ({
       id: event._id,
